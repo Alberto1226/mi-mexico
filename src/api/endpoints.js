7f9ff199f86dcfc5889d06c4feb91c1240903a12
Alberto1226 <a15021347@iteshu.edu.mx>
2023-06-15 12:09:18 -0600

    problemas de json resuletos

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -4,3 +4,6 @@ export const ENDPOINTLoginAdministrador = "/login";
 // Usuarios
 export const ENDPOINTRegistrarUsuario = "/usuarios/registro/";
 export const ENDPOINTObtenerUsuarios = "/usuarios/obtenerUsuario";
+
+// Peliculas
+export const ENDPOINTListarPeliculas = "/peliculas/listar";
---
+++ src/api/peliculasListar.js
@@ -0,0 +1,15 @@
+import {ENDPOINTListarPeliculas} from './endpoints';
+import axios from 'axios';
+import {API_HOST} from '../utils/constants';
+//import {getTokenApi} from ".auth"
+
+export async function listarPeliculas(){
+    const config = {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+           
+        }
+    };
+    return await axios.get(API_HOST + ENDPOINTListarPeliculas, config);
+}
