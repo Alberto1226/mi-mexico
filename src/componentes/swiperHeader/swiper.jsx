3c483ddc1a20396d01b71f277739ecf96403323e
Alberto1226 <a15021347@iteshu.edu.mx>
2023-07-21 13:45:30 -0600

    protector poster

--- src/componentes/swiperHeader/swiper.jsx
+++ src/componentes/swiperHeader/swiper.jsx
@@ -100,7 +100,7 @@ export function SwiperHeader(props) {
   return (
     <>
       <div className="headerVideo">
-        <video id="videoheader" src={props.videoh}  autoPlay playsinline loop muted className={`video-element ${showPoster ? 'show-poster' : ''}`}  ></video>
+        <video id="videoheader" src={props.videoh}  autoPlay playsinline loop muted className={`video-element ${showPoster ? 'show-poster' : ''}`}  poster={showPoster ? randomImages[randomIndex] : ''} ></video>
         <div className="areaswiper">
           
           <Swiper
